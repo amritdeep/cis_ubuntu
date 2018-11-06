6dc64c89e2b811ec0400be93c81d355f46ded425
Amrit Dhungana <amrit.dhungana@ibm.com@amrits-mbp.ustx.ibm.com>
2018-11-06 09:34:15 -0600

    Change default attri syntax

--- cis_ubuntu/attributes/default.rb
+++ cis_ubuntu/attributes/default.rb
@@ -1,9 +1,9 @@
 default['audit']['fetcher'] = 'chef-automate'
 default['audit']['reporter'] = 'chef-automate'
 
-default['audit']['profiles'] = [
+default[:audit][:profiles].push(
   {
     name: 'CIS Ubuntu Linux 16.04 LTS Benchmark Lever 1 -Server',
-    compliance: 'admin/cis-ubuntu16.04.lts-level1-server',
+    compliance: 'admin/cis-ubuntu16.04.lts-level1-server'
   },
-]
+)
